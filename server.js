c6fdcc1967ec1b6988a559de16caf1273e58e904
bobsyashi <113643287+bobsyashi@users.noreply.github.com>
2025-03-13 21:36:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express;
+const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.post('/button-click', (req, res) => {
+    console.log(req.body.message);
+    res.json({ status: 'success', message: 'Button click received!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
